307ed855590176f8ef4ac8da070a08756718ccd6
ZeroHints <137832954+ZeroHints@users.noreply.github.com>
2023-09-24 12:25:34 +0100

    Add volume

--- Run-Container.ps1
+++ Run-Container.ps1
@@ -8,7 +8,7 @@ $workingDir = Join-Path $PSScriptRoot $dockerName
 
 Push-Location $workingDir
 try {
-    docker run --rm -it "$($dockerName):dev"
+    docker run --rm -it -v lin-dev-box:/home/dev/mnt:rw --name lin-dev-box "$($dockerName):dev"
 }
 finally {
     Pop-Location
