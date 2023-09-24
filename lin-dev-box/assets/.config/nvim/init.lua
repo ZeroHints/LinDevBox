8d40b9d40e83ff08516ed1efc51ed2dc27c39d6f
ZeroHints <137832954+ZeroHints@users.noreply.github.com>
2023-09-24 21:48:10 +0100

    Update nvim options

--- lin-dev-box/assets/.config/nvim/init.lua
+++ lin-dev-box/assets/.config/nvim/init.lua
@@ -1,32 +1,5 @@
--- Options & Variables
-vim.opt.relativenumber = true
-vim.opt.number = true
-vim.opt.spell = false
-vim.opt.signcolumn = "auto"
-vim.opt.wrap = false
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-vim.g.autoformat_enabled = true
-vim.g.cmp_enabled = true
-vim.g.autopairs_enabled = true
-vim.g.diagnostics_mode = 3
-vim.g.icons_enabled = true
-vim.g.ui_notifications_enabled = true
-
-vim.g.netrw_keepdir = 0
-vim.g.netrw_winsize = 30
-vim.g.netrw_banner = 0
-vim.g.netrw_localcopydircmd = 0
-
 -- Plugins
-
--- Install package manager
---    https://github.com/folke/lazy.nvim --    `:help lazy.nvim.txt` for more info
+-- Install plugins manager (https://github.com/folke/lazy.nvim) `:help lazy.nvim.txt` for more info
 local lazyDirPath = vim.fn.stdpath('data')..'/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazyDirPath) then
   vim.fn.system({
@@ -41,14 +14,71 @@ end
 vim.opt.rtp:prepend(lazyDirPath)
 
 require('lazy').setup({
-  'nvim-lualine/lualine.nvim'
+  {
+    'nvim-lualine/lualine.nvim',
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = "onedark",
+        component_separators = "|",
+        section_separators = "",
+      }
+    }
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+    },
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ':TSUpdate',
+  },
 })
 
-require("lualine").setup({
-  options = {
-    icons_enabled = false,
-    theme = "onedark",
-    component_separators = "|",
-    section_separators = "",
-  }
-})
+-- Options & Variables
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.spell = false
+vim.opt.signcolumn = "auto"
+vim.opt.wrap = false
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.clipboard = 'unnamedplus' -- Sync clipboard between OS and Neovim.
+vim.opt.hlsearch = false -- Highlight on search
+vim.opt.breakindent = true -- Enable break indent
+vim.opt.undofile = true -- Save undo history
+vim.opt.ignorecase = true -- Case-insensitive searching UNLESS \C or capital in search
+vim.opt.smartcase = true -- Case-insensitive searching UNLESS \C or capital in search
+vim.opt.updatetime = 250 -- Decrease update time
+vim.opt.timeoutlen = 300 -- Decrease update time
+vim.opt.completeopt = 'menuone,noselect' -- Set completeopt to have a better completion experience
+vim.opt.termguicolors = true -- NOTE: You should make sure your terminal supports this
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.autoformat_enabled = true
+vim.g.cmp_enabled = true
+vim.g.autopairs_enabled = true
+vim.g.diagnostics_mode = 3
+vim.g.icons_enabled = true
+vim.g.ui_notifications_enabled = true
+
+vim.g.netrw_keepdir = 0
+vim.g.netrw_winsize = 30
+vim.g.netrw_banner = 0
+vim.g.netrw_localcopydircmd = 0
